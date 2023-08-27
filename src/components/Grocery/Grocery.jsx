c4492a4e7e2bdbc8fd7628638b9dc3ed60e7604a
Puja <pujak816@gmail.com>
2023-08-27 14:47:52 +0530

    commit changes

--- src/components/Grocery/Grocery.jsx
+++ src/components/Grocery/Grocery.jsx
@@ -4,12 +4,6 @@ const Grocery = () => {
   return (
     <div className="pt-20">
       <h1>Grocery Online</h1>
-      <div>
-        <li>Tomato</li>
-        <li>Rice</li>
-        <li>SOya</li>
-        <li>Chips</li>
-      </div>
     </div>
   );
 };
