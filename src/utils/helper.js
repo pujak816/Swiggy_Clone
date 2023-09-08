619911f6f08651269499b011b9279a793390bd6f
Puja <pujak816@gmail.com>
2023-09-08 23:17:29 +0530

    modified

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -3,8 +3,6 @@ export const FETCH_MENU_API =
 
 export const FETCH_RESDATA =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9566294&lng=77.70468230000002&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-// "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9566294&lng=77.70468230000002&page_type=DESKTOP_WEB_LISTING";
-// "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&page_type=DESKTOP_WEB_LISTING";
 
 export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
