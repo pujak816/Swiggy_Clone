be52a8001d47ba4cfc47ae742195249df65a9d26
Puja <pujak816@gmail.com>
2023-09-09 10:09:04 +0530

    fix: CORS ERROR

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,8 +1,17 @@
-export const FETCH_MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9566294&lng=77.70468230000002&restaurantId=";
+const resDataUurl =
+  "https://corsproxy.io/?" +
+  encodeURIComponent(
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9566294&lng=77.70468230000002&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  );
 
-export const FETCH_RESDATA =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9566294&lng=77.70468230000002&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const FETCH_RESDATA = resDataUurl;
+
+const menuApiUrl =
+  "https://corsproxy.io/?" +
+  encodeURIComponent(
+    "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9566294&lng=77.70468230000002&restaurantId="
+  );
+export const FETCH_MENU_API = menuApiUrl;
 
 export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
